646fefddd34873c2f1dc833d01fc6703e69a2d70
해슴 <42230703+RuinaR@users.noreply.github.com>
2024-03-04 12:59:45 +0900

    소드맨 작업

--- Project Knight/Assets/Scripts/Soldier.cs
+++ Project Knight/Assets/Scripts/Soldier.cs
@@ -2,16 +2,38 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Soldier : MonoBehaviour
 {
     public string unitName;
+
+    public int Hp { get; set; }
+    public int Damage { get; set; }
+    public int MoraleDamage { get; set; }
     public float Speed { get; set; }
+
+    private Soldier AttackTarget { get; set; } = null;
+
+    public void Initialize(int hp, int damage, int moraleDamage, float speed)
+    {
+        this.Hp = hp;
+        this.Damage = damage;
+        this.MoraleDamage = moraleDamage;
+        this.Speed = speed;
+    }
+
+    public void SetAttactTarget(Soldier target)
+    {
+       this.AttackTarget = target;
+    }
 
     public virtual void Attack()
     {
+        if(AttackTarget == null) { return; }
+        AttackTarget.TakeDamage(Damage); //MoraleDamage 보류
         //에니메이션 재생
-        //게임매니저의 체력바 깎기
+
     }
 
     public virtual void Move(int posX)
@@ -25,10 +47,28 @@ public class Soldier : MonoBehaviour
         transform.DOMoveX(posX, t);
     }
 
-    public virtual void Die()
+    private void Die()
     {
-        gameObject.SetActive(false);
-        //GameObject.Destroy(gameObject);
         //에니메이션 재생
+        float targetX = 50f;
+        if (transform.position.x < 0f)
+        { targetX *= -1f; }
+
+        DG.Tweening.Sequence sq = DOTween.Sequence();
+        sq.SetAutoKill()
+            .Append(transform.DOMove(new Vector3(targetX, 50f), 1f))
+            .AppendCallback(SetActiveFalse);
+        
+        //체력바 깎기 -100
+    }
+    private void SetActiveFalse()
+    { 
+        gameObject.SetActive(false); 
+    }
+    public void TakeDamage(int Damage)
+    {
+        Hp -= Damage;
+        if (Hp <= 0f)
+            Die();
     }
 }
---
+++ Project Knight/Assets/Scripts/SwordMan.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordMan : Soldier
+{
+    public override void Attack()
+    {
+        base.Attack();
+    }
+
+    public override void Move(int posX)
+    {
+        base.Move(posX);
+    }
+
+    private void Start()
+    {
+
+    }
+}
